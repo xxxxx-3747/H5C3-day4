f754ef2526136b2a9d79ac8854b4190b4df83742
xxxxx-3747 <1921708164@qq.com>
2023-08-16 12:40:09 +0800

    history/storage

---
+++ history/friend.php
@@ -0,0 +1,6 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+    $html = '朋友网页内容';
+    $page = 'friend';
+    include 'template.html'
+?>
---
+++ history/my.php
@@ -0,0 +1,6 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+    $html = '我的音乐网页内容';
+    $page = 'my';
+    include 'template.html'
+?>
